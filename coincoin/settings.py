35f1bd3fc93859b3ffa39f617f416768f3af4a72
nrurnru <naurud@gmail.com>
2019-12-18 23:41:00 +0900

    req

--- coincoin/settings.py
+++ coincoin/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'iu3((2)uzbe5@u_5_i1)4-yjje@1ckh=760ij63z00e%+fis1('
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['52.231.38.95']
+ALLOWED_HOSTS = ['52.231.38.95', '127.0.0.1']
 
 
 # Application definition
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+Static_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
