40675d476d1c6583d29cedaa8f788d6dba0c65ef
nrurnru <naurud@gmail.com>
2020-01-05 12:39:32 +0900

    enter page revised

--- coincoin/urls.py
+++ coincoin/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('community/', include('community.urls'))
+    path('', include('community.urls'))
 ]
